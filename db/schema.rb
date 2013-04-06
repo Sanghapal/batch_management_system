07027a646d9dc98efa61b3c2d047d4cb36ba9671
Radhika <priya.hagwane23@gmail.com>
2013-04-06 16:00:59 +0530

    modify gemfile and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130403081624) do
+ActiveRecord::Schema.define(:version => 20130403094145) do
 
   create_table "batches", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130403081624) do
   add_index "cities", ["state_id"], :name => "index_cities_on_state_id"
 
   create_table "countries", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -74,16 +75,6 @@ ActiveRecord::Schema.define(:version => 20130403081624) do
     t.integer  "country_id"
   end
 
-  create_table "student_grades", :force => true do |t|
-    t.integer  "student_id"
-    t.integer  "grade_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "student_grades", ["grade_id"], :name => "index_student_grades_on_grade_id"
-  add_index "student_grades", ["student_id"], :name => "index_student_grades_on_student_id"
-
   create_table "students", :force => true do |t|
     t.string   "first_name"
     t.string   "middle_name"
@@ -107,7 +98,6 @@ ActiveRecord::Schema.define(:version => 20130403081624) do
     t.boolean  "ban"
     t.string   "mobile"
     t.string   "alternate_mobile"
-    t.boolean  "band"
   end
 
   add_index "students", ["city_id"], :name => "index_students_on_city_id"
