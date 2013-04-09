1a660f12506f0a664c0efd855413daa6f2ef5242
Radhika <priya.hagwane23@gmail.com>
2013-04-09 12:13:10 +0530

    add sponsors model

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,13 @@ root :to => 'students#index'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+   
+resources :sponsors do
+    collection do
+      delete 'destroy_multiple'
+    end
+  end
+
    resources :grades do
     collection do
       delete 'multipledelete'
