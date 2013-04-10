0e0b8e6d429c84e77042186f7680141507df3ce0
vishal pawle <vishal@techvision.net.in>
2013-04-10 12:17:49 +0530

    before_filter has been aggains added in applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
 	    
   def load_country_state_city
     @countries = Country.order("name asc")
